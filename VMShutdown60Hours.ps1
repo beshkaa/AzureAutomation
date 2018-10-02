a3b0753f3f1567f13d3c42f75d76de35b48b1f94
Artem Philippov <artyom.v.filippov@gmail.com>
2018-10-02 15:22:18 +0100

    Initial commit

---
+++ VMShutdown60Hours.ps1
@@ -0,0 +1,55 @@
+workflow VMShutdown60Hours
+{
+param(
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+    [String]
+    $SubscriptionName
+    )
+
+
+$ErrorActionPreference = "silentlyContinue"   
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch 
+{
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+        }
+}
+
+Select-AzureRmSubscription -Subscription $SubscriptionName
+
+$vmList = Get-AzureRmResource -ResourceType Microsoft.Compute/virtualMachines
+
+$inlineDate = Get-Date
+$inlineDate = $inlineDate.AddHours(-60)
+
+ForEach -Parallel ($vmObject in $vmList) 
+{
+    if ((Get-AzureRMVm -Name $vmObject.Name -ResourceGroupName $vmObject.ResourceGroupName -Status).Statuses[1].Code -eq "Powerstate/running")
+    {
+        $vmLog = Get-AzureRMLog -StartTime $inlineDate -ResourceId $vmObject.Id | Where-Object {($_.Authorization.Action -eq "Microsoft.Compute/virtualMachines/start/action") -or ($_.Authorization.Action -eq "Microsoft.Compute/virtualMachines/write")}
+        if (![boolean]$vmLog)
+        {
+            Write-Output ($vmObject.Name +" should be powered off ")
+            Stop-AzureRmVM -ResourceGroupName $vmObject.ResourceGroupName -Name $vmObject.Name -Force
+        }
+    }   
+}
+}
---
+++ azure lab dev.ps1
@@ -0,0 +1,35 @@
+﻿# Enable parallel
+Workflow VmTracker
+{
+    param(
+         [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+         [String]
+         $SubscriptionName
+    )
+
+
+#   $SubscriptionName = "Enterprise - SE - Restore to Azure"
+    Select-AzureRmSubscription -Subscription $SubscriptionName
+
+    $vmList = Get-AzureRmResource -ResourceType Microsoft.Compute/virtualMachines
+
+    $inlineDate = Get-Date
+    $inlineDate = $inlineDate.AddHours(-60)
+
+    ForEach -Parallel ($vmObject in $vmList) 
+    {
+        if ((Get-AzureRMVm -Name $vmObject.Name -ResourceGroupName $vmObject.ResourceGroupName -Status).Statuses[1].Code -eq "Powerstate/running")
+        {
+           $vmLog = Get-AzureRMLog -StartTime $inlineDate -ResourceId $vmObject.Id | Where-Object {($_.Authorization.Action -eq "Microsoft.Compute/virtualMachines/start/action") -or ($_.Authorization.Action -eq "Microsoft.Compute/virtualMachines/write")}
+           if (![boolean]$vmLog)
+              {
+                  Write-Output "$($vmObject.Id) Should be powered off"
+               }
+       }
+    
+    
+    }
+
+}
+
+VmTracker -SubscriptionName "Enterprise - SE - Restore to Azure"-Verbose
