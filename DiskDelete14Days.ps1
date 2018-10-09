71fb2980ad952024c8b81b204fb71ce2116f1758
Artem Philippov <artyom.v.filippov@gmail.com>
2018-10-09 07:08:03 +0100

    production

--- DiskDelete14Days.ps1
+++ DiskDelete14Days.ps1
@@ -1,3 +1,61 @@
-workflow DiskDelete14Days
+
+#Delete disk after 14 Days of Last Start/Allocation state.
+
+Workflow DiskDelete14Days
 {
+    param(
+         [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+         [String]
+         
+         $SubscriptionName
+         )
+
+    $InformationPreference = "Continue"
+    $WarningPreference = "SilentlyContinue"
+    $ErrorActionPreference = "Continue"   
+    $connectionName = "AzureRunAsConnection"
+
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    
+        "Logging in to Azure..."
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch 
+    {
+            if (!$servicePrincipalConnection)
+            {
+                $ErrorMessage = "Connection $connectionName not found."
+                throw $ErrorMessage
+            } else {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+            }
+    }
+
+    Select-AzureRmSubscription -Subscription $SubscriptionName
+
+    $diskList = Get-AzureRmDisk
+    $inlineDate = (Get-Date).AddDays(-14)
+    
+    ForEach -Parallel ($diskObject in $diskList) 
+    {
+        if ($diskObject.ManagedBy -eq $null)
+        {
+           $vmLog = (Get-AzureRMLog -StartTime $inlineDate.DateTime -ResourceId $diskObject.Id).Authorization | Where-Object -Property Action -in -Value "Microsoft.Compute/disks/write"
+           if (![boolean]$vmLog)
+           {
+                Write-Output "$($diskObject.DiskSizeGB)GB - $($diskObject.Id) should be deleted"
+                $diskObject | Remove-AzureRmDisk -Force
+           }
+       }
+       
+    }
+
 }
