3136f0dc98e962ee077247a67b1c3086c854f0a7
Artem Philippov <artyom.v.filippov@gmail.com>
2018-10-23 14:02:37 +0100

    Ready to production

--- StorageAccountDelete14Days.dev.ps1
+++ StorageAccountDelete14Days.dev.ps1
@@ -15,31 +15,38 @@ Workflow StorageAccountDelete14Days {
     $WarningPreference = "SilentlyContinue"
     $ErrorActionPreference = "Continue" 
 
-    $SubscriptionName = "Enterprise - SE - Restore to Azure" #[<----- dev] 
-    # $exceptionList = @("0365diag242","emo")
+    #[<----- dev] 
+    $SubscriptionName = "Enterprise - SE - Restore to Azure" 
     
     Select-AzureRmSubscription -Subscription $SubscriptionName
-    $inlineDate = (Get-Date).AddDays(-1)
+    $inlineDate = (Get-Date).AddDays(-14)
     $storageAccountList = Get-AzureRmStorageAccount
 
+
+    
     ForEach -Parallel ($storageAccountObject in $storageAccountList) {
         if (($storageAccountObject.CreationTime -lt $inlineDate) -and ($exceptionList -notcontains $storageAccountObject.StorageAccountName)) {
-            Write-Output "$($storageAccountObject.StorageAccountName) time $($storageAccountObject.CreationTime)"
             
             $storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccountObject.ResourceGroupName -Name $storageAccountObject.StorageAccountName)[0].Value
        
             # Context is desearilized -- here and after not able to store in variable 
             $containerList = Get-AzureStorageContainer -Context (New-AzureStorageContext -StorageAccountName $storageAccountObject.StorageAccountName -StorageAccountKey $storageKey)
-     
-            ForEach -Parallel ($containerObject in $containerList) {
+            
+            #Removing all containers older then 14 days without VHD inside. VHD Deletion is handled as part of DiskDelete job, as more complecated
+            ForEach ($containerObject in $containerList) {
                 $blobList = Get-AzureStorageBlob -Container $containerObject.Name -Context (New-AzureStorageContext -StorageAccountName $storageAccountObject.StorageAccountName -StorageAccountKey $storageKey)
-                if ($blobList.Name -notmatch "\.vhd" )  {
-                    Write-Output "Deleting $($storageAccountObject.StorageAccountName)"
+                if (($containerObject.LastModified -lt $inlineDate) -and !($blobList.Name -match "\.vhd" )) {
+                    Write-Output "$($containerObject.LastModified):$($storageAccountObject.StorageAccountName)\$($containerObject.name) - should be deleted - $(Remove-AzureStorageContainer -Name $containerObject.Name -Context (New-AzureStorageContext -StorageAccountName $storageAccountObject.StorageAccountName -StorageAccountKey $storageKey) -Force -WhatIf)"
                 }
-                
-            }    
+            }
+          
+            #Removing empty storage accounts
+            $containerList = (Get-AzureStorageContainer -Context (New-AzureStorageContext -StorageAccountName $storageAccountObject.StorageAccountName -StorageAccountKey $storageKey))
+            if (!$containerList) {
+                Write-Output "$($storageAccountObject.StorageAccountName) - should be deleted - $(Remove-AzureRmStorageAccount -Name $storageAccountObject.StorageAccountName -ResourceGroupName $storageAccountObject.ResourceGroupName -Force -WhatIf)"
+            }
         }
     }
 }
 
-StorageAccountDelete14Days -SubscriptionName "Enterprise - SE - Restore to Azure" -exceptionList emeaprgpersistent
+StorageAccountDelete14Days -SubscriptionName "Enterprise - SE - Restore to Azure" -exceptionList emeaprgpersistent, demoazurerestore
